25a363e67421fbd87296bfb942a8ddc0eba027b5
Ryne Lyons <ryne@vegapunk.collin.txlyons.com>
2025-12-30 19:04:03 -0600

    Obstacle Scrips in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,20 +9,30 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
 
     [SerializeField] private MeshRenderer floorMeshRenderer;
-    
+
     [HideInInspector] public bool gameStarted = false;
-    [Header("Speed")]
-    public float startingSpeed = 0.5f;
+    [Header("Speed")] public float startingSpeed = 0.5f;
     public float speedIncreasePerSecond = 0.1f;
     public float currentScoreIncreaseSpeed = 2f;
-    [Header("Text")]
-    public TextMeshProUGUI scoreText;
+    [Header("Text")] public TextMeshProUGUI scoreText;
+    
+    [Header("Obstacle Spawn")] 
+    public float timeDelayBetweenObstacle = 1f;
+    [Space]
+    public GameObject[] allGroundObstacles;
+    public GameObject[] allFlyingObstacles;
+    [Space]
+    public Transform groundObstacleSpawnPoint;
+    public Transform flyingObstacleSpawnPoint;
+    
+    private List<GameObject> allCurrentObstacles = new List<GameObject>();
+    
+    private float currentSpeed;
     
     private int highScore = 0;
     private float currentScore = 0;
     
-    
-    private float currentSpeed;
+    private float timeUntilNextObstacle = 1f;
     
     private void Awake()
     {
@@ -42,6 +52,42 @@ public class GameManager : MonoBehaviour
     {
         if (gameStarted)
         {
+            timeUntilNextObstacle -= Time.deltaTime;
+
+            if (timeUntilNextObstacle <= 0f)
+            {
+                timeUntilNextObstacle = timeDelayBetweenObstacle;
+                
+                //Spawn new obstacle
+                if(currentScore >= 50)
+                {
+                    //random spawn ground or air
+                    if (UnityEngine.Random.value > 0.8F) //20 % Chance
+                    {
+                        //spawn air
+                        GameObject newObstacle = Instantiate(allFlyingObstacles[UnityEngine.Random.Range(0, allFlyingObstacles.Length)], flyingObstacleSpawnPoint.position, Quaternion.identity);
+                        allCurrentObstacles.Add(newObstacle);
+                    }
+                }
+                else
+                {
+                    //spawn ground
+                    GameObject newObstacle = Instantiate(allGroundObstacles[UnityEngine.Random.Range(0, allGroundObstacles.Length)], groundObstacleSpawnPoint.position, Quaternion.identity);
+                    allCurrentObstacles.Add(newObstacle);
+                }
+            }
+            else
+            {
+                //spawn ground
+                GameObject newObstacle = Instantiate(allGroundObstacles[UnityEngine.Random.Range(0, allGroundObstacles.Length)], groundObstacleSpawnPoint.position, Quaternion.identity);
+                allCurrentObstacles.Add(newObstacle);
+            }
+
+            foreach (GameObject _obstacle in allCurrentObstacles)
+            {
+                _obstacle.transform.Translate(new Vector3(-currentSpeed * Time.deltaTime, 0, 0));
+            }
+            
             currentSpeed += speedIncreasePerSecond * Time.deltaTime;
             
             floorMeshRenderer.material.mainTextureOffset += new Vector2(currentSpeed * Time.deltaTime, 0);
